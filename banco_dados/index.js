479f3c759b82370be45be7feec2d69b241dc67fc
prof-ronny <carlos.sousa34@fatec.sp.gov.br>
2023-09-22 16:10:50 -0300

    Atualiza banco

--- banco_dados/index.js
+++ banco_dados/index.js
@@ -8,6 +8,7 @@ const instancia = new Sequelize(
     config.get('mysql.senha'),
     {
         host: config.get('mysql.host'),
+        port:config.get('mysql.porta'),
         dialect: 'mysql'
     }
 )
